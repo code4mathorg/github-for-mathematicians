5e6264fe5016c622c039c813bfe9f9bde5c3207c
Steven Clontz <steven.clontz@gmail.com>
2023-08-04 16:02:00 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -33,8 +33,8 @@ var ptx_lunr_docs = [
   "url": "ch-git-github.html",
   "type": "Chapter",
   "number": "1",
-  "title": "Git vs. GitHub asdfsadfas",
-  "body": " Git vs. GitHub asdfsadfas   What Is Git?    An illustration of Git history   An illustration of a project's history controlled by Git     Git is a distributed version control system that tracks changes in any set of computer files. sadfsadf Git was originally authored by Linus Torvalds in 2005 for development of the Linux kernel. Git is free and open-source software shared under the GPL-2.0-only license.  Two core concepts of Git are commits (illustrated in by circles) and branches (illustrated in by lines).    What Is Git Hub ?  Hello   "
+  "title": "Git vs. GitHub",
+  "body": " Git vs. GitHub   What Is Git?    An illustration of Git history   An illustration of a project's history controlled by Git    Git is a distributed version control system that tracks changes in any set of computer files. Git was originally authored by Linus Torvalds in 2005 for development of the Linux kernel. Git is free and open-source software.  Two core concepts of Git are commits (illustrated in by circles) and branches (illustrated in by lines). A commit represents the state of of your project at a particular point in its history. Branches allow this history to not be linear: you can branch off to experiment on a particular new feature, then merge this feature branch back into the main branch when it's complete. This is particularly useful when multiple people collaborate ( ) on a Git-managed project.  A Git project is often called a repository , or repo for short.    What Is Git Hub ?  Another key feature of Git is the ability to share your project, along with its history, with other people. This is generally accomplished by hosting your repository on a service such as GitHub : GitHub.com . (Other such services include BitBucket.org and GitLab.com .)  We'll use GitHub not only to host our repositories, but also because it provides all the tools needed to author content using only a web browser: no need for multiple installations or memorizing incantations like git commit -m \"foobar\" to type into a command line prompt.    An example  An example of a project using Git and GitHub is the document you're reading right now! This project is open-sourced and shared at , and was created completely using the tools described here.   "
 },
 {
   "id": "fig-git-branches",
@@ -64,13 +64,49 @@ var ptx_lunr_docs = [
   "body": "commits branches "
 },
 {
+  "id": "p-11",
+  "level": "2",
+  "url": "ch-git-github.html#p-11",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "repository repo "
+},
+{
+  "id": "p-12",
+  "level": "2",
+  "url": "ch-git-github.html#p-12",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "GitHub "
+},
+{
   "id": "ch-first-repo",
   "level": "1",
   "url": "ch-first-repo.html",
   "type": "Chapter",
   "number": "2",
   "title": "Your First Repository",
-  "body": " Your First Repository  Hello   Creating the repo  Hello    Using GitHub.dev  Hello    Commiting and syncing  Hello   "
+  "body": " Your First Repository   Making an account  All the features of GitHub we'll be using are available using a free GitHub account. Make your account by visiting .  Many mathematicians are also eligible for GitHub's educator discount, which provides additional functionality and computational resources normally only available to Pro users. Visit while logged into your account to request it. (You do not need to wait for approval before continuing to the next section.)    Creating the repo  Once logged in, a new repository can be created by pressing the + button in the toolbar, or visiting .  The repository will need a name, which can be something like my-first-repo for this tutorial. (GitHub will also suggest a cute random name like ubiquitous-space-tribble if you have writer's block.)  Repositories can be public to everyone on the internet or private to only people you approved. I encourage you to work publicly, to make it easier to collaborate with the open-source community I can personally attest to publishing many garbage repositories on GitHub (along with my hopefully-useful ones), and no one has called me out for it yet!  The last option we'll make sure to select is to Initialize this repository with: Add a README file . Then click Create repository .    Editing README.md  While logged into GitHub.com, you have the ability to edit individual files on your repositories. (If your repository is public, others can see those files, but cannot edit them unless you make them a collaborator, see .)  An easy way to edit an individual file is just to click the pencil icon such as the one that appears on your README. This file is written in Markdown , a markup language that takes plain text like *this* and renders it like this .  Try to edit your file to say something like I'm learning how to use GitHub!     Using GitHub.dev  Hello    Commiting and syncing  Hello   "
+},
+{
+  "id": "p-19",
+  "level": "2",
+  "url": "ch-first-repo.html#p-19",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "public private "
+},
+{
+  "id": "p-22",
+  "level": "2",
+  "url": "ch-first-repo.html#p-22",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Markdown "
 },
 {
   "id": "ch-github-pages",
@@ -97,7 +133,25 @@ var ptx_lunr_docs = [
   "type": "Chapter",
   "number": "5",
   "title": "Open Educational Resources",
-  "body": " Open Educational Resources  Hello   PreTeXt  Hello    CheckIt  Hello   "
+  "body": " Open Educational Resources  Hello   PreTeXt  Hello    A sample interactive   Parsons Problem, Mathematical Proof even numbers  Create a proof of the theorem: If is an even number, then .    Suppose is even.     Then is a prime number.    Then there exists an so that .    Then there exists an so that .     Click the heels of your ruby slippers together three times.    So we have the displayed equation: .  This is a superfluous second paragraph in this block.    Thus .   Dorothy will not be much help with this proof.    CheckIt  Hello   "
+},
+{
+  "id": "fig-geogebra",
+  "level": "2",
+  "url": "ch-oer.html#fig-geogebra",
+  "type": "Figure",
+  "number": "5.1.1",
+  "title": "",
+  "body": "  A sample interactive  "
+},
+{
+  "id": "number-theory-proof",
+  "level": "2",
+  "url": "ch-oer.html#number-theory-proof",
+  "type": "Checkpoint",
+  "number": "5.1.2",
+  "title": "Parsons Problem, Mathematical Proof.",
+  "body": "Parsons Problem, Mathematical Proof even numbers  Create a proof of the theorem: If is an even number, then .    Suppose is even.     Then is a prime number.    Then there exists an so that .    Then there exists an so that .     Click the heels of your ruby slippers together three times.    So we have the displayed equation: .  This is a superfluous second paragraph in this block.    Thus .   Dorothy will not be much help with this proof. "
 },
 {
   "id": "ch-collaboration",
